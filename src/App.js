0e553960c68d801f210d213bc002bd2cad0e537f
周先军 <alone@zhouxianjundeMacBook-Air.local>
2018-12-05 21:55:31 +0800

    add tabbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Router, Stack, Scene } from 'react-native-router-flux';
+import {
+    Router, Stack, Scene, Tabs
+} from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import Login from './views/login';
 import Home from './views/home';
 
 export default function () {
     return (
         <Router>
-            <Stack key="root">
-                <Scene key="login" component={Login} title="登录" />
-                <Scene key="home" component={Home} initial />
-            </Stack>
+            <Scene key="root" hideNavBar>
+                <Tabs key="tabs" tabs>
+                    <Scene
+                        key="home"
+                        component={Home}
+                        tabBarLabel="首页"
+                        initial
+                        icon={() => <Icon name="home" size={20} />}
+                    />
+                    <Scene
+                        key="user"
+                        component={Login}
+                        tabBarLabel="我的"
+                        icon={() => <Icon name="user" size={20} />}
+                    />
+                </Tabs>
+                <Stack key="routers">
+                    <Scene key="login" component={Login} title="登录" />
+                </Stack>
+            </Scene>
         </Router>
     );
 }
--- src/views/home.js
+++ src/views/home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import TabBar from 'antd-mobile-rn';
+import { TabBar, SearchBar } from 'antd-mobile-rn';
+
+const HomeIcon = require('../../assets/home.png');
+const MyIcon = require('../../assets/my.png');
 
 export default () => (
-    <TabBar>
-        <TabBar.Item title="首页">
-
-        </TabBar.Item>
-        <TabBar.Item title="订单">
-
-        </TabBar.Item>
-    </TabBar>
+    <View>
+        <SearchBar placeholder="搜索" />
+        {/* <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="#ccc"
+        >
+            <TabBar.Item title="首页" icon={HomeIcon}></TabBar.Item>
+            <TabBar.Item title="我的" icon={MyIcon}></TabBar.Item>
+        </TabBar> */}
+    </View>
 );
