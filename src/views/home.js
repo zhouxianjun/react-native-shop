58756dd7b3790b7767e4966c66c22c46ac5998ee
周先军 <alone@zhouxianjundeMacBook-Air.local>
2018-12-06 21:13:19 +0800

    user

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+    View, Text, FlatList, StyleSheet
+} from 'react-native';
 import { SearchBar } from 'antd-mobile-rn';
 import { Actions } from 'react-native-router-flux';
 
+const styles = StyleSheet.create({
+    cateItem: {
+        height: 40,
+        fontSize: 18,
+        padding: 10
+    }
+});
+const data = Array.from({ length: 30 }).map((item, index) => Object.assign({
+    key: `key-${index}`,
+    name: `name-${index + 1}`
+}));
 export default () => (
-    <View>
+    <View style={{ flex: 1 }}>
         <SearchBar placeholder="搜索" />
-        <Text onPress={() => Actions.login()}>去登录</Text>
+        <View style={{ flex: 1 }}>
+            <FlatList data={data} renderItem={({ item }) => <Text style={styles.cateItem}>{`aa${item.name}`}</Text>} />
+        </View>
     </View>
 );
--- src/views/user.js
+++ src/views/user.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+import UserAvatar from 'react-native-user-avatar';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import { Card } from 'antd-mobile-rn';
 
 export default function () {
     return (
-        <Text>Alone</Text>
+        <View style={{ height: 'auto' }}>
+            <View style={{
+                height: 150, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center'
+            }}
+            >
+                <UserAvatar size="50" name="Alone" src="https://avatars2.githubusercontent.com/u/5254523?s=400&u=03d3b6870da5dc89eabf15e4c06e6a32b95a0ffa&v=4" />
+                <Text style={{ fontSize: 16, color: '#fff' }}>Alone</Text>
+            </View>
+            <Card full style={{ marginTop: 10 }}>
+                <Card.Header title="我的订单" style={{ height: 40 }}></Card.Header>
+                <Card.Body>
+                    <View style={{ justifyContent: 'center', flexDirection: 'row' }}>
+                        <View style={{ flex: 1, alignItems: 'center' }}>
+                            <Icon name="list-alt" size={50} />
+                            <Text>全部订单</Text>
+                        </View>
+                        <View style={{ flex: 1, alignItems: 'center' }}>
+                            <Icon name="bars" size={50} />
+                            <Text>代支付</Text>
+                        </View>
+                    </View>
+                </Card.Body>
+            </Card>
+        </View>
     );
 }
