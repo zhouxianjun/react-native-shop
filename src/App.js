83c2d1e45f94225cca9b611dbdf26aa0befe8dfe
Alone <715213471@qq.com>
2018-12-06 17:53:06 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import {
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import Login from './views/login';
 import Home from './views/home';
+import User from './views/user';
 
 export default function () {
     return (
         <Router>
-            <Scene key="root" hideNavBar>
-                <Tabs key="tabs" tabs>
+            <Stack key="root">
+                <Tabs key="tabs" hideNavBar>
                     <Scene
                         key="home"
                         component={Home}
                         tabBarLabel="首页"
+                        title="首页"
                         initial
                         icon={() => <Icon name="home" size={20} />}
                     />
                     <Scene
                         key="user"
-                        component={Login}
+                        component={User}
                         tabBarLabel="我的"
                         icon={() => <Icon name="user" size={20} />}
                     />
                 </Tabs>
-                <Stack key="routers">
-                    <Scene key="login" component={Login} title="登录" />
-                </Stack>
-            </Scene>
+                <Scene key="login" component={Login} title="登录" failure={() => false} on={() => false} />
+            </Stack>
         </Router>
     );
 }
--- src/views/home.js
+++ src/views/home.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { SearchBar } from 'antd-mobile-rn';
 import { Actions } from 'react-native-router-flux';
-import { TabBar, SearchBar } from 'antd-mobile-rn';
-
-const HomeIcon = require('../../assets/home.png');
-const MyIcon = require('../../assets/my.png');
 
 export default () => (
     <View>
         <SearchBar placeholder="搜索" />
-        {/* <TabBar
-            unselectedTintColor="#949494"
-            tintColor="#33A3F4"
-            barTintColor="#ccc"
-        >
-            <TabBar.Item title="首页" icon={HomeIcon}></TabBar.Item>
-            <TabBar.Item title="我的" icon={MyIcon}></TabBar.Item>
-        </TabBar> */}
+        <Text onPress={() => Actions.login()}>去登录</Text>
     </View>
 );
